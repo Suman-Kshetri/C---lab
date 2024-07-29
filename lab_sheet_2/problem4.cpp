eb34f0c4bbf1edaca00992714ccf73cc1d9c74c2
Suman-Kshetri <ksuman20601028@gmail.com>
2024-07-29 19:17:11 +0545

    sum of two time and the sum and difference between two time

--- lab_sheet_2/problem4.cpp
+++ lab_sheet_2/problem4.cpp
@@ -6,123 +6,113 @@
 // - overload > function to compare two time
 // - member function to display time in HH:MM:SS format
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class TIME{
-    int hours,minute,second;
-    public:
-    TIME(){
-        hours=0;
-        minute=0;
-        second=0;
+class TIME {
+    int hours, minutes, seconds; // Correct variable name 'minutes'
+
+public:
+    // Default constructor
+    TIME() : hours(0), minutes(0), seconds(0) {}
+
+    // Parameterized constructor
+    TIME(int hrs, int mins, int sec) : hours(hrs), minutes(mins), seconds(sec) {}
+
+    // Overload > operator
+    bool operator>(const TIME& time2) const {
+        if (hours > time2.hours) return true;
+        if (hours < time2.hours) return false;
+        if (minutes > time2.minutes) return true;
+        if (minutes < time2.minutes) return false;
+        return seconds > time2.seconds;
     }
-    TIME(int hrs,int mins,int sec)
-    {
-        hours=hrs;
-        minute=mins;
-        second=sec;
+
+    // Overload < operator
+    bool operator<(const TIME& time2) const {
+        if (hours < time2.hours) return true;
+        if (hours > time2.hours) return false;
+        if (minutes < time2.minutes) return true;
+        if (minutes > time2.minutes) return false;
+        return seconds < time2.seconds;
     }
-    TIME operator + (TIME time2)
-    {
+
+    // Overload == operator
+    bool operator==(const TIME& time2) const {
+        return hours == time2.hours && minutes == time2.minutes && seconds == time2.seconds;
+    }
+
+    // Overload + operator
+    TIME operator+(const TIME& time2) const {
         TIME time;
-        time.hours=hours+time2.hours;
-        time.minute=minute+time2.minute;
-        time.second=second+time2.second;
-        while(time.second>=60)
-        {
-            time.minute+=1;
-            time.second-=60;
-        }
-        while(time.minute>=60)
-        {
-            time.minute+=1;
-            time.minute-=60;
-        }
+        time.seconds = seconds + time2.seconds;
+        time.minutes = minutes + time2.minutes + time.seconds / 60;
+        time.hours = hours + time2.hours + time.minutes / 60;
+        time.seconds %= 60;
+        time.minutes %= 60;
         return time;
     }
-    TIME operator - (TIME time2)
-    {
+
+    // Overload - operator
+    TIME operator-(const TIME& time2) const {
         TIME time;
-        time.hours=hours-time2.hours;
-        time.minute=minute-time2.minute;
-        time.second=second-time2.second;
-        if(time.hours<0)
-        time.hours=(-1)*time.hours;
-        else if(time.minute<0)
-        time.minute=(-1)*time.minute;
-        else if(time.second<0)
-        time.second=(-1)*time.second;
+        int totalSeconds1 = hours * 3600 + minutes * 60 + seconds;
+        int totalSeconds2 = time2.hours * 3600 + time2.minutes * 60 + time2.seconds;
+        int diff = totalSeconds1 - totalSeconds2;
+
+        if (diff < 0) diff = -diff; // Make sure diff is positive
+
+        time.hours = diff / 3600;
+        time.minutes = (diff % 3600) / 60;
+        time.seconds = diff % 60;
+
         return time;
     }
-    void display()
-    {
-        cout<<"TIME : "<<hours<<" hrs : "<<minute<<" min : "<<second<< " sec "<<endl;
+
+    // Display function
+    void display() const {
+        cout << "TIME : "
+             << (hours < 10 ? "0" : "") << hours << " hrs : "
+             << (minutes < 10 ? "0" : "") << minutes << " min : "
+             << (seconds < 10 ? "0" : "") << seconds << " sec" << endl;
     }
-    
-    bool operator > (TIME time2)
-{
-    
-    if(hours>time2.hours)
-    return true;
-    else if(minute>time2.minute)
-    return true;
-    else if(second>time2.second)
-    return true;
-}
-
-    bool operator < (TIME time2)
-{
-    
-    if(hours<time2.hours)
-    return true;
-    else if(minute<time2.minute)
-    return true;
-    else if(second<time2.second)
-    return true;
-}
-
-    bool operator == (TIME time2)
-{
-    
-    if(hours==time2.hours)
-    return true;
-    else if(minute==time2.minute)
-    return true;
-    else if(second==time2.second)
-    return true;
-}
 };
 
+int main() {
+    int hrs1, min1, sec1;
+    int hrs2, min2, sec2;
 
-int main(){
-    
-    int hrs1,min1,sec1;
-    int hrs2,min2,sec2;
-    cout<<"Enter the First time: "<<endl;
-    cout<<"Hours: ";
-    cin>>hrs1;
-    cout<<"Minute: ";
-    cin>>min1;
-    cout<<"SECOND: ";
-    cin>>sec1;
-    cout<<"Enter the Second time: "<<endl;
-    cout<<"Hours: ";
-    cin>>hrs2;
-    cout<<"Minute: ";
-    cin>>min2;
-    cout<<"SECOND: ";
-    cin>>sec2;
-    TIME time1(hrs1,min1,sec1),time2(hrs2,min2,sec2),sum,sub;
-    if(time1>time2)
-        cout<<"First time is greater than second time "<<endl;
-    else if(time1<time2)
-        cout<<"Second time is greater than first time "<<endl;
-    else if(time1==time2)
-        cout<<"Both the time are eqaull";
-    sum=time1+time2;
+    cout << "Enter the First time: " << endl;
+    cout << "Hours: ";
+    cin >> hrs1;
+    cout << "Minute: ";
+    cin >> min1;
+    cout << "Second: ";
+    cin >> sec1;
+
+    cout << "Enter the Second time: " << endl;
+    cout << "Hours: ";
+    cin >> hrs2;
+    cout << "Minute: ";
+    cin >> min2;
+    cout << "Second: ";
+    cin >> sec2;
+
+    TIME time1(hrs1, min1, sec1), time2(hrs2, min2, sec2);
+    TIME sum = time1 + time2;
+    TIME sub = time1 - time2;
+
+    if (time1 > time2)
+        cout << "First time is greater than second time " << endl;
+    else if (time1 < time2)
+        cout << "Second time is greater than first time " << endl;
+    else
+        cout << "Both the times are equal" << endl;
+
+    cout << "Sum of times: ";
     sum.display();
-    sub=time1-time2;
+    cout << "Difference of times: ";
     sub.display();
+
     return 0;
 }
